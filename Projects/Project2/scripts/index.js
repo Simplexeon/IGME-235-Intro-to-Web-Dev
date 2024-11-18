cab7f98317974a57189258f38ed357975f417475
Simplexeon <gamerguy941mc@gmail.com>
2024-11-18 14:44:55 -0500

    Monster display updates

--- Projects/Project2/scripts/index.js
+++ Projects/Project2/scripts/index.js
@@ -1,17 +1,25 @@
 
 
 // Pexels API paEVAyCYoDaeRIfffGm61640fmNct6Mz9owiPixlpC1OROYiRufPY5OP
-const DND_QUERY = "https://api.open5e.com/v1/monsters/?search=tarrasque";
+const DND_MONSTER_QUERY = "https://api.open5e.com/v1/monsters/?limit=10&search=";
 const RNG_QUERY = "http://www.randomnumberapi.com/api/v1.0/random?min=1&max=20&count=10";
 
 
-const combatOrder = [];
-
 window.onload = init;
+
+// Data
+
+const combatOrder = [];
+const monsters = [];
+
+let searchTerm;
+let lastResults = [];
+
+
 
 function init() {
     document.querySelector("#open5e-button").onclick = dndPressed;
-
+	addEventListener("keydown", monsterSearch)
 }
 
 function updateCombatOrderDisplay() {
@@ -19,11 +27,75 @@ function updateCombatOrderDisplay() {
 
     let orderString = "";
 
-    for(let combatUnit in order) {
-        order[combatUnit]["name"]
+    for(let combatUnit in combatOrder) {
+		orderString += "<div class=\"combat-unit\"><h3>" + combatOrder[combatUnit]["name"] + "</h3><hr>";
+		orderString += "<div class=\"stats\"><h4>Initiative: " + combatOrder[combatUnit]["initiative"] + "</h4><h4>Health: ";
+		orderString += combatOrder[combatUnit]["hp"] + "</h4></div>";
     }
 
+	co.innerHTML = orderString;
 }
+
+function monsterSearch(e) {
+	if(e.target != document.querySelector("#enemy-search")) {
+		return;
+	}
+	searchTerm = e.target.value;
+	
+	xmlQuery(DND_MONSTER_QUERY + searchTerm).onload = searchResults;
+}
+
+function searchResults(e) {
+	let searchResults = document.querySelector("#search-results");
+	while (searchResults.firstChild) {
+		searchResults.removeChild(searchResults.firstChild);
+	}
+	
+	let response = JSON.parse(e.target.response);
+	let results = response["results"];
+	
+	for(let monster in results) {
+		let button = document.createElement("button");
+		button.className = "result";
+		button.dataset.monster = results[monster];
+		
+		button.innerHTML = "<p>" + results[monster]["name"] + "</p><em>" + results[monster]["document__title"] + "</em></button>";
+		
+		searchResults.appendChild(button);
+		button.addEventListener("click", (e) => addMonster(results[monster]));
+	}
+	
+	lastResults = results;
+}
+
+function addMonster(monster) {
+	monsters.push(monster);
+	console.log(monster);
+	updateMonsterDisplay();
+}
+
+function updateMonsterDisplay() {
+	let monsterDisplay = document.querySelector("#enemies")
+	
+	let longString = "";
+	
+	for(monster of monsters) {
+		longString += "<div class=\"combat-unit\"><h3>" + monster["name"] + "</h3><hr>";
+		longString += "<ul class=\"stats\"><li><h4>Str</h4><p>" + monster["strength"] + "</p></li>";
+		longString += "<li><h4>Dex</h4><p>" + monster["dexterity"] + "</p></li>";
+		longString += "<li><h4>Con</h4><p>" + monster["constitution"] + "</p></li>";
+		longString += "<li><h4>Int</h4><p>" + monster["intelligence"] + "</p></li>";
+		longString += "<li><h4>Wis</h4><p>" + monster["wisdom"] + "</p></li>";
+		longString += "<li><h4>Cha</h4><p>" + monster["charisma"] + "</p></li></ul>";
+		longString += "<div class=\"tankyness\"><div><h4>HP</h4><p>" + monster["hit_points"] + "</p></div>";
+		longString += "<div><h4>AC</h4><p>" + monster["armor_class"] + "</p></div></div>";
+		
+		longString += "</div>";
+	}
+	
+	monsterDisplay.innerHTML = longString;
+}
+
 
 function dndPressed(e) {
     xmlQuery(DND_QUERY).onload = dndResult;
